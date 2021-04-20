fc3b4a3674c62445fb8951c0baaeb06e43d3f58b
Hanastasiah <anastasia.thambwe.ser@gmail.com>
2021-04-20 10:35:47 +0200

    Commenting and addition of obstacles
    Adding comments to the main class, adding new obstacle objects. One commented for plausible random apparition of objects in the middle of the game screen, the other commented as a basis for the wall obstacles

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ FramePerSec = pygame.time.Clock()
 
 
 # Other Variables for use in the program
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 700
-SPEED = 5
-SCORE = 0
+BLACK = (0, 0, 0) #defines the colour black in a variable
+WHITE = (255, 255, 255) #defines the colour white in a variable
+SCREEN_WIDTH = 500 #defines the width of the game screen
+SCREEN_HEIGHT = 700 #defines the height of the game screen
+SPEED = 5 #defines the general speed of movement
+SCORE = 0 #Variable representing the score (initialised at 0 for the beginning of the game)
 
 # Setting up Fonts
 font = pygame.font.SysFont("Verdana", 60)
@@ -35,19 +35,25 @@ pygame.display.set_caption("Group 15")
 
 
 # Setting up Sprites
-P1 = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+P1 = Player(SCREEN_WIDTH, SCREEN_HEIGHT) #defines the player object
 
-E1 = Obstacle(SCREEN_WIDTH,SCREEN_HEIGHT,SPEED)
+E1 = Obstacle(SCREEN_WIDTH,SCREEN_HEIGHT,SPEED) #defines the original obstacles object spawing when we enter a new "screen scene" in the game
 
-back_ground = Background(SCREEN_WIDTH,SCREEN_HEIGHT,DISPLAYSURF)
+#E2 = Obstacle(SCREEN_WIDTH,SCREEN_HEIGHT/2,SPEED) #obstacle that could be used for spawning random obstacles (e.g. vortex)
+
+#E3 = Obstacle(SCREEN_WIDTH,SCREEN_HEIGHT+350,SPEED)
+
+back_ground = Background(SCREEN_WIDTH,SCREEN_HEIGHT,DISPLAYSURF) #defines the background object
 
 # Creating Sprites Groups
 obstacles = pygame.sprite.Group()
 obstacles.add(E1)
+#obstacles.add(E2)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
+#all_sprites.add(E2)
 
 # Adding a new User event: how often to look for user event?
 INC_SPEED = pygame.USEREVENT + 1
@@ -69,8 +75,8 @@ while True:
 
     # DISPLAYSURF.blit(background, (0,0))
     #display a score
-    scores = font_small.render(str(SCORE), True, BLACK)
-    DISPLAYSURF.blit(scores, (10, 10))
+    scores = font_small.render(str(SCORE), True, BLACK) #defines the font properties of the score
+    DISPLAYSURF.blit(scores, (10, 10)) #displays the score in the top left corner of the screen
 
     # Moves and Re-draws all Sprites
     for entity in all_sprites:
@@ -83,15 +89,15 @@ while True:
         #pygame.mixer.Sound('crash.wav').play()
         time.sleep(0.8)
 
-        DISPLAYSURF.fill(BLACK)
-        DISPLAYSURF.blit(game_over, (SCREEN_WIDTH/10, SCREEN_HEIGHT/4))
+        DISPLAYSURF.fill(BLACK) #Fills screen with black, shows a black screen
+        DISPLAYSURF.blit(game_over, (SCREEN_WIDTH/10, SCREEN_HEIGHT/4)) #Writes game over on the screen on top of the black screen
 
         pygame.display.update()
         for entity in all_sprites:
             entity.kill()
-        time.sleep(1.5)
-        pygame.quit()
-        sys.exit()
+        time.sleep(1.5) #wait one and a half second
+        pygame.quit() #quit pygame
+        sys.exit() #close the demo window
 
     pygame.display.update()
     FramePerSec.tick(FPS)
